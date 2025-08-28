06e1a5d41f444295e4c07b38286d1c410165f414
Alejandro Hernandez Carrillo <el_grande_ahc@hotmail.com>
2025-08-28 10:23:35 -0600

    S12:  Pipes numericos, de fecha y de texto

--- pipes-app/src/app/components/basic-page/basic-page.component.ts
+++ pipes-app/src/app/components/basic-page/basic-page.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
   styles: ``
 })
 export default class BasicPageComponent {
+ nameLower = signal('alejandro');
+ nameUpper = signal('Alejandro');
+ fullName = signal('AlEjAnDrO HerNaNdEz');
 
+ customDate = signal(new Date());
+
+ tickingDateEffect = effect((onCleanup) => {
+  const interval = setInterval(() => {
+    console.log("tick");    
+   this.customDate.set(new Date());
+  }, 1000);
+  onCleanup(() => clearInterval(interval)); // MUY IMPORTANTE IMPLEMENTAR ESTE CLEANUP PARA EVITAR FUGAS DE MEMORIA
+ });
 }
