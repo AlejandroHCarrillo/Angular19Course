9112d6ad412fe58cdd5a39735d84866412de4440
Alejandro Hernandez Carrillo <el_grande_ahc@hotmail.com>
2025-09-01 14:06:37 -0600

    S13: Crear firnulario usando form group, form controls y formControlName

--- reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,17 @@
 import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'basic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.component.html',
 })
-export class BasicPageComponent { }
+export class BasicPageComponent {
+  myForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    price: new FormControl(0, [Validators.required, Validators.min(0)]),
+    storage: new FormControl(0, [Validators.required, Validators.min(0)])
+  });
+
+}
