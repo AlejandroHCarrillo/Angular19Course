3f2fdcc837b425fb332c59cf38f1fcab84137d9d
Alejandro Hernandez Carrillo <el_grande_ahc@hotmail.com>
2025-09-02 13:24:57 -0600

    S14: Agregar y eliminar controles dinamicamente

--- reactive-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ reactive-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,9 +1,75 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'dynamic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './dynamic-page.component.html',
 })
-export class DynamicPageComponent { }
+export class DynamicPageComponent {
+  private fb = inject(FormBuilder);
+  formUtils = FormUtils;
+  
+
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array([
+      ['Resident evil', [Validators.required, Validators.minLength(4)]],
+      ['Mario Kart', [Validators.required, Validators.minLength(2)]]
+    ], Validators.minLength(3))
+  });
+
+  newFavorite = new FormControl('', [Validators.required, Validators.minLength(2)]); // control independiente para agregar un control al formulario pricipal
+
+  get favoriteGames(){
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  onAddToFavorites(){
+    // console.log("agregando a favoritos");
+    // console.log('is valid?' , !this.newFavorite.invalid);
+    // console.log('errors?' , this.newFavorite.errors );
+    
+    if(this.newFavorite.invalid) return;
+
+    const newGame = this.newFavorite.value;
+    
+    console.log({newGame});
+    
+    // agregamos el nuevo juego a arreglo de juegos favoritos
+    this.favoriteGames.push(this.fb.control(newGame, [Validators.required, Validators.minLength(2) ] ) );
+
+    // Limpiamos el texto del input para agregar favoritos
+    this.newFavorite.reset();
+
+  }
+
+  onDeleteFavorite(index: number){
+    this.favoriteGames.removeAt(index);
+  }
+
+  onSave(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    const fg = this.myForm.controls['favoriteGames'] as FormArray;
+    if(fg.controls.length === 0 ) {
+      // agregamos el nuevo juego a arreglo de juegos favoritos
+      const newGame = this.newFavorite.value;
+      this.favoriteGames.push(this.fb.control(newGame, [Validators.required, Validators.minLength(2) ] ) );      
+      return;
+    }
+    
+    console.log('Formulario enviado', this.myForm.value);
+    // console.log(fg.controls.length);
+
+    this.myForm.reset({ name: '', price: 0, inStorage: 0 });
+    this.myForm.controls['favoriteGames'].reset();
+
+    this.favoriteGames.clear();
+  }
+ }
--- reactive-forms-app/src/app/utils/form-utils.ts
+++ reactive-forms-app/src/app/utils/form-utils.ts
@@ -1,4 +1,4 @@
-import { FormGroup } from "@angular/forms";
+import { FormArray, FormGroup, ValidationErrors } from "@angular/forms";
 
 export class FormUtils {
     
@@ -10,20 +10,54 @@ export class FormUtils {
   }
 
   static getFieldError(form: FormGroup, fieldName: string): string | null {
+    if (!form.controls[fieldName]) return null;
+
     const errors = form.controls[fieldName].errors;
     if (errors) {
-      for (const error in errors) {
-        if (errors['required']) {
-          return 'Este campo es obligatorio';
-        }
-        if (errors['minlength']) {
-          return `Este campo debe tener al menos ${errors['minlength'].requiredLength} caracteres`;
-        }
-        if (errors['min']) {
-          return 'Este campo debe ser mayor o igual a ' + errors['min'].min;
-        }
-      }
+        return this.getErrorDescription(errors);
     }
+    return null;
+  }
+
+  static isValidFieldInArray(formArray: FormArray, index : number){
+    return !( formArray.controls[index].errors && formArray.controls[index].touched ); 
+  }     
+
+  static getFieldErrorInArray(FormArray: FormArray, index: number): string | null {
+    if (FormArray.controls.length === 0) return null;
+
+    const errors = FormArray.controls[index].errors;
+
+    if (errors) {
+        return this.getErrorDescription(errors);
+    }
+    return null;
+  }
+
+  private static getErrorDescription(errors: ValidationErrors): string | null{
+    // console.log(errors);
+    // for (const error in errors) {
+    //     if (errors['required']) {
+    //       return 'Este campo es obligatorio.';
+    //     }
+    //     if (errors['minlength']) {
+    //       return `Este campo debe tener al menos ${errors['minlength'].requiredLength} caracteres.`;
+    //     }
+    //     if (errors['min']) {
+    //       return `Este campo debe ser mayor o igual a  ${errors['min'].min}.`;
+    //     }
+    //   }
+    // Usando switch y recorriendo las llaves del objeto Object.keys
+    for (const key of Object.keys(errors)) {
+        switch(key){
+            case 'required': 
+                return 'Este campo es obligatorio.';
+            case 'minlength': 
+                return `Este campo debe tener al menos ${errors['minlength'].requiredLength} caracteres.`;
+            case 'min':  
+                return `Este campo debe ser mayor o igual a  ${errors['min'].min}.`;
+        }
+    } 
     return null;
   }
 }
