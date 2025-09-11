5b4b64b68b877d5af71fb6ea81ade273df91cfa6
Alejandro Hernandez Carrillo <el_grande_ahc@hotmail.com>
2025-09-11 14:08:04 -0600

    Cambiar los nombres de los folders de los proyectos

---
+++ 05 reactive-forms-app/src/app/shared/components/side-menu/side-menu.component.ts
@@ -0,0 +1,36 @@
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { reactiveRoutes } from '../../../reactive/reactive.routes';
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  title: string;
+  route: string;
+}
+
+const reactiveItems = reactiveRoutes[0].children ?? [];
+
+@Component({
+  selector: 'side-menu',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './side-menu.component.html',
+})
+export class SideMenuComponent {
+  reactiveMenu: MenuItem[] = reactiveItems
+  .filter(item => item.path !== '**') // eliminamos la ruta no deseada '**' 
+  .map(({ title, path }) => (
+    { 
+      title: `${title}`, 
+      route: `reactive/${path}` 
+    }));
+
+    authMenu: MenuItem[] =  [{
+      title: 'Registro',
+      route: './auth'
+    }]
+
+    countryMenu: MenuItem[] =  [{
+      title: 'Paises',
+      route: 'country'
+    }];
+
+}
